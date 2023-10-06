a57f4e781f873c5c727cac689edff09b29c1e49b
Petr Horáček <petr.horacek@edgy.digital>
2023-10-06 12:26:18 +0200

    Badge ui fix

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export default function Badge({className = '', text, Icon, dotClassName = ''}: Props) {
   return (
-    <div className={`flex h-[16px] items-center rounded-full px-x ${className}`}>
+    <div className={`flex h-[16px] items-center rounded-full bg-white px-x shadow-default4 ${className}`}>
       <div className={`mr-x h-[4px] w-[4px] rounded-full bg-black ${dotClassName}`} />
       {Icon}
       <p className={`ml text-[8px] leading-[8px] ${Icon ? 'ml-x' : ''}`}>{text}</p>
